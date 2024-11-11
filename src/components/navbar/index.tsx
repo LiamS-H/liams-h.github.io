7193490888cc11d9d402e8f7a23b3a55ba4224e8
LiamS-H <liamsh@gmail.com>
2024-11-11 12:56:22 -0800

    add navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,37 @@
+import { NavLink, useLocation } from "react-router-dom";
+import { routes } from "../../routes";
+import Hitbox from "../hitbox";
+
+function NavBarButton(props: { path: string; label: string; active: boolean }) {
+    return (
+        <NavLink to={props.path}>
+            <Hitbox
+                id={props.path}
+                className={`rounded p-4 mb-4 ${
+                    props.active
+                        ? "bg-black text-purple-600"
+                        : "bg-black text-white hover:text-pink-500"
+                }`}
+            >
+                <li key={props.path}>{props.label}</li>
+            </Hitbox>
+        </NavLink>
+    );
+}
+
+export default function NavBar() {
+    const location = useLocation();
+    return (
+        <nav className="fixed bottom-0 w-full">
+            <ul className="flex justify-around">
+                {routes.map((r) => (
+                    <NavBarButton
+                        path={r.path}
+                        label={r.label}
+                        active={location.pathname === r.path}
+                    />
+                ))}
+            </ul>
+        </nav>
+    );
+}
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Water from "../components/water";
 import { WindowFocusProvider } from "../contexts/focus";
+import NavBar from "../components/navbar";
 
 export default function Root() {
     return (
         <WindowFocusProvider>
             <Water>
                 <Outlet />
+                <NavBar />
             </Water>
         </WindowFocusProvider>
     );
