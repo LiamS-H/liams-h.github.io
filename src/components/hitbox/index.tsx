e221afdcdf59262d4dbc4712e47698df8fbb736e
LiamS-H <liamsh@gmail.com>
2024-11-11 12:58:14 -0800

    fix hitbox

--- src/components/hitbox/index.tsx
+++ src/components/hitbox/index.tsx
@@ -1,21 +1,23 @@
-import { ReactNode, useRef } from "react";
-import { useFluidRegister } from "../../contexts/fluid";
+import { HTMLProps, useRef } from "react";
+import { useFluidBoundRegister } from "../../contexts/fluid";
 
-export default function Hitbox(props: { id: string; children?: ReactNode }) {
+export default function Hitbox(
+    props: { id: string; innnerBounds?: true } & HTMLProps<HTMLDivElement>
+) {
     const boundRef = useRef<HTMLDivElement | null>(null);
     // const boundRect = useMemo(
     //     () => boundRef.current?.getBoundingClientRect(),
     //     [boundRef.current]
     // );
-    let boundRect = undefined;
-    if (boundRef.current) {
+    let boundRect = boundRef.current?.getBoundingClientRect();
+    if (boundRef.current && props.innnerBounds) {
         const range = document.createRange();
         range.selectNodeContents(boundRef.current);
         boundRect = range.getBoundingClientRect();
         range.detach();
     }
 
-    useFluidRegister(boundRect, props.id);
+    useFluidBoundRegister(boundRect, props.id);
 
-    return <div ref={boundRef}>{props.children}</div>;
+    return <div className="w-fit h-fit" ref={boundRef} {...props} />;
 }
