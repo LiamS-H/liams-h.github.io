12f74416799423e5b42b63bc44044ee0514a8697
liamS-H <liamsh@gmail.com>
2024-11-12 21:03:31 -0800

    reformat projects

--- src/routes/Projects/index.tsx
+++ src/routes/Projects/index.tsx
@@ -7,7 +7,7 @@ function Card(props: HTMLProps<HTMLDivElement> & { id: string }) {
         <Hitbox
             {...props}
             className={
-                "w-1/4 min-w-[300px] h-fit bg-black rounded p-4 m-4 flex-col flex"
+                "w-1/12 aspect-square min-w-[300px] h-fit bg-black rounded p-4 m-4 flex-col flex"
             }
             id={props.id}
         >
@@ -21,13 +21,7 @@ export default function Projects() {
         <div className="h-full flex flex-col align-middle justify-center">
             <div className="h-fit flex flex-row justify-around align-middle flex-wrap">
                 <Card id="Scrycards">
-                    <h1>Scrycards</h1>
-                    <ExternalLink href="https://github.com/LiamS-H/react-scrycards">
-                        Github
-                    </ExternalLink>
-                    <ExternalLink href="https://card-confluence.web.app/about">
-                        Example site
-                    </ExternalLink>
+                    <h1 className="pb-1">Scrycards</h1>
                     <p>
                         Wraps{" "}
                         <ExternalLink href="https://scryfall.com/docs/api">
@@ -37,17 +31,27 @@ export default function Projects() {
                         rendering the returned data. Dependency injection allows
                         for using custom caches, or API wrappers / endpoints.
                     </p>
+                    <div className="flex justify-center flex-row space-x-4">
+                        <ExternalLink href="https://github.com/LiamS-H/react-scrycards">
+                            Github
+                        </ExternalLink>
+                        <ExternalLink href="https://card-confluence.web.app/about">
+                            Example site
+                        </ExternalLink>
+                    </div>
                 </Card>
                 <Card id="FOOOD">
                     <h1>Food ML</h1>
-                    <ExternalLink href="https://devpost.com/software/foooood">
-                        Devpost
-                    </ExternalLink>
                     <p>
                         A machine learning full stack project for estimating
                         caloric data from images. This project was a 1st place
                         winner at HackMecedIX.
                     </p>
+                    <div className="flex justify-center flex-row space-x-4">
+                        <ExternalLink href="https://devpost.com/software/foooood">
+                            Devpost
+                        </ExternalLink>
+                    </div>
                 </Card>
             </div>
         </div>
