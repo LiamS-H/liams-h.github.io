d6d73baccec39e48f1f316ea51afff224a7ae4b3
liams-h <liamsh@gmail.com>
2025-02-01 13:38:53 -0800

    cleanup remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // base: process.env.NODE_ENV === "production" ? "/liams-h.github.io/" : "/",
     base: "/",
     plugins: [react()],
     resolve: {
