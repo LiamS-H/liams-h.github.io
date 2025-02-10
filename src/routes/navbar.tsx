66f794495ed0485d3883cd45b267c0b9e8e899a0
liams-h <liamsh@gmail.com>
2025-02-09 18:56:48 -0800

    add navbar color to subroutes

--- src/routes/navbar.tsx
+++ src/routes/navbar.tsx
@@ -2,11 +2,21 @@ import { NavLink, useLocation } from "react-router-dom";
 import { routes } from ".";
 import Hitbox from "../components/hitbox";
 
-function NavBarButton(props: { path: string; label: string; active: boolean }) {
+function NavBarButton(props: {
+    path: string;
+    label: string;
+    active: null | boolean;
+}) {
     return (
         <NavLink
             to={props.path}
-            className={props.active ? "cursor-default" : undefined}
+            className={
+                props.active === null
+                    ? undefined
+                    : props.active
+                    ? "cursor-default"
+                    : undefined
+            }
         >
             <Hitbox
                 id={props.path}
@@ -14,7 +24,7 @@ function NavBarButton(props: { path: string; label: string; active: boolean }) {
                     props.active
                         ? " text-purple-600"
                         : " text-white hover:text-pink-500"
-                }`}
+                } ${props.active === null ? "text-orange-400" : ""}`}
             >
                 <li key={props.path}>{props.label}</li>
             </Hitbox>
@@ -22,8 +32,22 @@ function NavBarButton(props: { path: string; label: string; active: boolean }) {
     );
 }
 
+function match(path: string, path2: string) {
+    if (path2 === "/") {
+        return path === path2;
+    }
+    if (path === path2) {
+        return true;
+    }
+    if (path.startsWith(path2)) {
+        return null;
+    }
+    return false;
+}
+
 export default function NavBar() {
     const { pathname } = useLocation();
+
     return (
         <nav className="fixed bottom-0 w-full md:px-[20%]">
             <ul className="flex justify-around">
@@ -32,7 +56,7 @@ export default function NavBar() {
                         key={r.path}
                         path={r.path}
                         label={r.label}
-                        active={pathname === r.path}
+                        active={match(pathname, r.path)}
                     />
                 ))}
             </ul>
