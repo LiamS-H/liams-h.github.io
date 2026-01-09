4fc3bcaa83ab565f5bebaada37a76a7519f6dbbf
liams-h <liamsh@gmail.com>
2026-01-08 21:47:43 -0800

    add image lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()]
 });
