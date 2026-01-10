882235e0321d374970fbd75a0bb85cf070b33421
liams-h <liamsh@gmail.com>
2026-01-10 14:20:16 -0800

    add solid bound clipping

--- src/lib/components/fluid/solid.svelte.ts
+++ src/lib/components/fluid/solid.svelte.ts
@@ -1,37 +1,42 @@
 import { useFluidContext } from '$lib/context/fluid.svelte';
+import { constrainRects as constrainRect } from '$lib/utils/constraint';
 import type { Action } from 'svelte/action';
 
-export const registerSolid: Action<HTMLElement, { id: string; color?: number; inner?: number }> = (
-	node,
-	params
-) => {
+export const registerSolid: Action<
+	HTMLElement,
+	{ id: string; color?: number; inner?: number; parent?: HTMLElement }
+> = (node, params) => {
 	const fluid = useFluidContext();
+	let current_params = params;
 
 	const controller = new AbortController();
 
-	const update = ({ id, color, inner }: typeof params) => {
+	const update = (new_params: typeof params) => {
+		const { id, color, inner, parent } = new_params;
+		current_params = new_params;
 		const rect = node.getBoundingClientRect();
+		const pRect = (parent || node.parentElement)?.getBoundingClientRect();
+		if (pRect) constrainRect(rect, pRect);
 		fluid.registerBound(rect, id, color);
 		if (!inner) return;
-		fluid.registerBound(
-			{
-				x: rect.x + inner,
-				y: rect.y + inner,
-				height: rect.height - 2 * inner,
-				width: rect.width - 2 * inner
-			},
-			id + '-inner'
-		);
+		const iRect = {
+			x: rect.x + inner,
+			y: rect.y + inner,
+			height: rect.height - 2 * inner,
+			width: rect.width - 2 * inner
+		};
+		if (pRect) constrainRect(iRect, pRect);
+		fluid.registerBound(iRect, id + '-inner');
 	};
 
-	const observer = new ResizeObserver(() => update(params));
+	const observer = new ResizeObserver(() => update(current_params));
 	observer.observe(node);
-	window.addEventListener('scroll', () => update(params), {
+	window.addEventListener('scroll', () => update(current_params), {
 		capture: true,
 		passive: true,
 		signal: controller.signal
 	});
-	window.addEventListener('resize', () => update(params), {
+	window.addEventListener('resize', () => update(current_params), {
 		passive: true,
 		signal: controller.signal
 	});
@@ -40,7 +45,8 @@ export const registerSolid: Action<HTMLElement, { id: string; color?: number; in
 		update,
 		destroy: () => {
 			observer.disconnect();
-			fluid.registerBound(null, params.id);
+			fluid.registerBound(null, current_params.id);
+			fluid.registerBound(null, current_params.id + '-inner');
 			controller.abort();
 		}
 	};
