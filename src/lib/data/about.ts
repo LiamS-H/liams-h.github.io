7548fec469c0696c7b7f8ae952f7826d2142909d
liams-h <liamsh@gmail.com>
2026-01-11 23:16:30 -0800

    add about bio

---
+++ src/lib/data/about.ts
@@ -0,0 +1,25 @@
+`
+I specialize in Typescript and React but the
+last 3 years I've also been chasing my other
+passion working as a whitewater rafting and
+outdoor guide. A river is a special place and it
+brings me great joy to see eddies and vortices
+wrap around the elements on this page.
+Hydrodynamics is not usually a concern when
+working on a site layout, but here we are!
+
+My personal projects over the last few years
+have all followed a similar suit, being mostly
+practical and related to things I'm interested
+in at the time. My recent fixation has been a
+little ecosystem of projects related to Magic
+The Gathering; this culminated in a{" "}
+<InternalLink to="/projects/shahrazad">
+    table-top simulator
+</InternalLink>{" "}
+that I use to play Magic with my friends a few
+nights a week.
+
+While I currently live near Sacramento, CA, I'm
+happy to relocate. Looking at SF.
+`;
