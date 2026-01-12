14dc99b7627bd6fad5fda6076b7a82a10c5ba6df
liams-h <liamsh@gmail.com>
2026-01-11 23:16:08 -0800

    deprecate _old

--- _old/src/routes/Home/index.tsx
+++
@@ -1,7 +0,0 @@
-import { useFluidColorRegister, useFluidTextRegister } from "@/contexts/fluid";
-
-export default function Home() {
-    useFluidColorRegister(0);
-    useFluidTextRegister("Liam");
-    return null;
-}
--- _old/src/types/fluid.ts
+++
@@ -1,9 +0,0 @@
-export interface FluidRect {
-    x: number;
-    y: number;
-    w: number;
-    h: number;
-}
-export type FluidRectList = [number, number, number, number][];
-
-export type FluidRects = Map<string, FluidRect | null>;
