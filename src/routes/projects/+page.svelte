5dd63ea30781efbd70b5cbb6ef4c60dd2c8ca557
liams-h <liamsh@gmail.com>
2026-01-11 20:10:05 -0800

    add stateful carousel

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,30 +1,131 @@
 <script lang="ts">
 	import { useFluidContext } from '$lib/context/fluid.svelte';
-	import { onMount } from 'svelte';
+	import { onMount, tick } from 'svelte';
 	import { routes } from '$lib/data/routes';
 	import { project_ids } from '$lib/data/projects';
 	import ProjectCard from './project-card.svelte';
 
 	const fluid = useFluidContext();
 
+	let behavior: ScrollBehavior = $state('instant');
+	let activeIndex: number = $state(0);
+	let carouselEl: HTMLUListElement;
+	let scrollTimeout: ReturnType<typeof setTimeout>;
+
+	function updateActiveState() {
+		if (!carouselEl) return;
+
+		const containerCenter = carouselEl.scrollLeft + carouselEl.offsetWidth / 2;
+
+		let i = 0;
+
+		for (const child of Array.from(carouselEl.children)) {
+			if (child.tagName === 'LI') {
+				const card = child as HTMLLIElement;
+				const cardCenter = card.offsetLeft - 128.5 + card.offsetWidth / 2;
+				const distance = Math.abs(containerCenter - cardCenter);
+
+				if (distance == 0) {
+					activeIndex = i;
+					sessionStorage.setItem('carousel_index', i.toString());
+					return;
+				}
+				i += 1;
+			}
+		}
+	}
+
+	function scroll(direction: -1 | 1, wrap = true) {
+		if (wrap) {
+			activeIndex = (project_ids.length + activeIndex + direction) % project_ids.length;
+		} else {
+			activeIndex = Math.max(0, Math.min(activeIndex + direction, project_ids.length - 1));
+		}
+		sessionStorage.setItem('carousel_index', activeIndex.toString());
+	}
+
 	onMount(() => {
 		fluid.registerText('');
 		fluid.changeColor(routes[2].color);
+
+		activeIndex = Number(sessionStorage.getItem('carousel_index'));
+		const controller = new AbortController();
+
+		window.addEventListener(
+			'wheel',
+			(e: WheelEvent) => {
+				scroll(0 < e.deltaY ? 1 : -1, false);
+			},
+			{ passive: true, signal: controller.signal }
+		);
+		window.addEventListener(
+			'scroll',
+			() => {
+				clearTimeout(scrollTimeout);
+				scrollTimeout = setTimeout(updateActiveState, 50);
+			},
+			{
+				passive: true,
+				signal: controller.signal,
+				capture: true
+			}
+		);
+
+		tick().then(() => (behavior = 'smooth'));
+
+		return () => {
+			controller.abort();
+			clearTimeout(scrollTimeout);
+		};
 	});
 </script>
 
-<div class="h-full px-16 md:px-32 flex flex-col justify-center">
+<div class="relative h-full flex flex-col justify-center px-10 md:px-32">
+	<button
+		class="absolute left-4 top-1/2 lg:top-2/3 lg:-translate-x-14 lg:left-1/2 z-10 -translate-y-1/2 text-white/50 transition-colors hover:text-white"
+		onclick={() => scroll(-1)}
+		aria-label="Scroll left"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-12 w-12"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			stroke-width="1.5"
+		>
+			<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+		</svg>
+	</button>
 	<div class="flex flex-col-reverse justify-center">
 		<ul
+			bind:this={carouselEl}
 			class="h-fit gap-8 md:gap-24 flex flex-row items-center overflow-x-auto snap-x snap-mandatory md:snap-normal
                     mask-[linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]
                     [webkit-mask:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]"
+			style="scrollbar-width: none;"
 		>
 			<div class="min-w-[40%] h-full"></div>
-			{#each project_ids as id}
-				<ProjectCard {id} active={false} />
+			{#each project_ids as id, i}
+				<ProjectCard {id} active={activeIndex === i} {behavior} />
 			{/each}
 			<div class="min-w-[40%] h-full"></div>
 		</ul>
 	</div>
+	<button
+		class="absolute right-4 top-1/2 z-10 lg:top-2/3 lg:left-1/2 -translate-y-1/2 text-white/50 transition-colors hover:text-white"
+		onclick={() => scroll(1)}
+		aria-label="Scroll right"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-12 w-12"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+			stroke-width="1.5"
+		>
+			<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+		</svg>
+	</button>
 </div>
--- src/routes/projects/project-card.svelte
+++ src/routes/projects/project-card.svelte
@@ -3,38 +3,63 @@
 	import { type ProjectID, projects } from '$lib/data/projects';
 	import { gradients } from '$lib/utils/colors';
 
-	const { id, active }: { id: ProjectID; active: boolean } = $props();
+	const {
+		id,
+		active,
+		behavior = 'smooth'
+	}: { id: ProjectID; active: boolean; behavior?: ScrollBehavior } = $props();
 	const project = projects[(() => id)()];
+	let bind: HTMLElement;
+
+	$effect(() => {
+		if (active) bind.scrollIntoView({ behavior, inline: 'center' });
+	});
 </script>
 
 <li
-	class={`1w-60 min-w-60 h-60 min-h-60 mx-4 p-4 text-transparent bg-clip-text ${gradients[project.colorNum]} animate-gradient-swirl flex flex-col justify-between snap-center`}
+	bind:this={bind}
+	class="relative flex h-60 min-h-60 w-60 min-w-60 snap-center flex-col justify-between mx-4 select-none"
 	use:registerSolid={{
 		id: `project-card-${id}`,
 		color: project.colorNum,
 		inner: 4
 	}}
+	{id}
 >
-	<h1 class="text-4xl pt-2 text-[2.15rem]">{project.title}</h1>
-	<p>{project.intro.summary}</p>
-	<i>{project.intro.hook}</i>
-	<div class="flex align-middle justify-center flex-row space-x-4">
-		{#if project.liveLink}
+	<button
+		type="button"
+		class="absolute inset-0 z-0 cursor-pointer"
+		aria-label={`Scroll project ${project.title} into view`}
+		onclick={() => {
+			bind.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+		}}
+	></button>
+
+	<div
+		class={`pointer-events-none relative z-10 p-4 flex h-full flex-col justify-between animate-gradient-swirl bg-clip-text text-transparent ${gradients[project.colorNum]}`}
+	>
+		<h1 class="text-[2.15rem] pt-2 text-4xl">{project.title}</h1>
+		<p>{project.intro.summary}</p>
+		<i>{project.intro.hook}</i>
+
+		<div class="pointer-events-auto flex flex-row justify-center space-x-4 align-middle">
+			{#if project.liveLink}
+				<a
+					class="bg-size-[200%_200%] bg-left transition-all duration-300 hover:bg-right bg-clip-text text-transparent bg-linear-to-r from-pink-500 to-purple-600"
+					href={project.liveLink}
+				>
+					Visit Site
+				</a>
+			{:else if project.githubLink}
+				<a href={project.liveLink}> Github </a>
+			{/if}
 			<a
-				class="bg-linear-to-r from-pink-500 to-purple-600 bg-size-[200%_200%] bg-left hover:bg-right transition-all duration-300 text-transparent bg-clip-text"
-				href={project.liveLink}
+				class="bg-size-[300%_100%] bg-left transition-all duration-1500 hover:bg-right bg-clip-text text-transparent bg-linear-to-r from-color-3 to-color-2"
+				href={`/projects/${project.id}`}
+				data-sveltekit-preload-data="hover"
 			>
-				Visit Site
+				Explore
 			</a>
-		{:else if project.githubLink}
-			<a href={project.liveLink}> Github </a>
-		{/if}
-		<a
-			class="text-transparent bg-linear-to-r from-color-3 to-color-2 bg-size-[300%_100%] bg-left hover:bg-right transition-all duration-1500 bg-clip-text"
-			href={`/projects/${project.id}`}
-			data-sveltekit-preload-data="hover"
-		>
-			Explore
-		</a>
+		</div>
 	</div>
 </li>
