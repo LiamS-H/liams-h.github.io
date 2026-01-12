a8f830e265b76b0113e624d8895323df8ea504d4
liams-h <liamsh@gmail.com>
2026-01-12 14:19:39 -0800

    add improved technology colors

--- src/lib/components/technology.svelte
+++ src/lib/components/technology.svelte
@@ -8,10 +8,10 @@
 
 	function getColor(_types: typeof type) {
 		const types = _types as readonly string[];
-		if (types.includes('Framework')) return 6;
-		if (types.includes('CICD')) return 5;
+		if (types.includes('Framework')) return 3;
+		if (types.includes('CICD')) return 1;
 		if (types.includes('Database')) return 4;
-		if (types.includes('Library')) return 3;
+		if (types.includes('Library')) return 2;
 		return 1;
 	}
 	const color = $derived(getColor(type));
