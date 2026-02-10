74b0f7284a3fd0260f5f6a8538fa4d254a3bc043
liams-h <liamsh@gmail.com>
2026-02-09 22:11:07 -0800

    improve gradients

--- src/lib/components/fluid/sim/shaders/utils.ts
+++ src/lib/components/fluid/sim/shaders/utils.ts
@@ -21,13 +21,13 @@ export namespace FUNCTION {
 	export const smokeColor = /*wgsl*/ `
     fn smokeColor(c: i32, y_ratio: f32) -> vec3<f32> {
         if (c == 0) {
-            return vec3<f32>(1.0, y_ratio, 0.5 +y_ratio*0.5);
+            return vec3<f32>(1.0, y_ratio*y_ratio, 0.5+y_ratio*0.5);
         } else if (c == 1) {
             return vec3<f32>(1.0, 0.8-y_ratio * 0.8, 0.0);
         } else if (c == 2) {
             return vec3<f32>(y_ratio, 0.8 + 0.2 * y_ratio, y_ratio * 0.2);
         } else if (c == 3) {
-            return vec3<f32>(y_ratio, 1.0, 1.0);
+            return vec3<f32>(y_ratio, 0.5 + y_ratio * 0.5, 0.5 + y_ratio * 0.5);
         } else if (c == 4) {
             return vec3<f32>(1.0, y_ratio * 0.5, y_ratio * 0.5);
         } else if (c == 5) {
