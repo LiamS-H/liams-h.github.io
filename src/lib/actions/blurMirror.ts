da92340238f68a015c8116030da9f1326e34406f
liamsh <liamsh@gmail.com>
2026-02-23 21:36:24 -0800

    make buttons prettier - ty grace :)

--- src/lib/actions/blurMirror.ts
+++ src/lib/actions/blurMirror.ts
@@ -6,7 +6,14 @@ export function blurMirror(node: HTMLElement) {
 
 	const mirror = document.createElement('div');
 	mirror.className =
-		'fixed pointer-events-none backdrop-blur-xs rounded-full z-0 will-change-transform top-0 left-0';
+		'fixed pointer-events-none rounded-full z-0 will-change-transform top-0 left-0 overflow-hidden';
+
+	const glass = document.createElement('div');
+	glass.className =
+		'absolute -inset-4 backdrop-blur-xl [mask-image:radial-gradient(closest-side,transparent_0%,black_80%,black_100%)]';
+	// 'absolute -inset-4 mask-image:radial-gradient(closest-side,transparent_40%,black_100%)';
+
+	mirror.appendChild(glass);
 	main.prepend(mirror);
 
 	let rafId: number;
--- src/lib/components/transparent-link.svelte
+++ src/lib/components/transparent-link.svelte
@@ -7,7 +7,7 @@
 
 <a
 	{...anchor}
-	class="flex items-center gap-2 rounded-full p-4 text-xl text-white shadow-lg transition-colors duration-300 hover:bg-white/5 hover:text-white/50 active:bg-purple-700/20"
+	class="flex items-center gap-2 rounded-full p-4 text-xl text-white/80 shadow-lg transition-colors duration-300 hover:bg-white/10 hover:text-white active:bg-purple-700/20"
 	use:blurMirror
 >
 	{@render children?.()}
